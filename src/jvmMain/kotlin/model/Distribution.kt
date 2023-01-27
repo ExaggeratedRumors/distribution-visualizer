69ecba50d9fde6b28f29a024172dc44b239e73cf
exaggeratedrumors <exaggeratedrumors@gmail.com>
2023-01-27 18:29:01 +0100

    Extend distributions

--- src/jvmMain/kotlin/model/Distribution.kt
+++ src/jvmMain/kotlin/model/Distribution.kt
@@ -3,31 +3,40 @@ package model
 import kotlin.math.*
 
 interface Distribution {
-    fun genRange(): FloatArray
-    fun probabilityDensityFunction(x: Int): Float
+    fun takeRange(): List<Float>
+    fun probabilityDensityFunction(x: Float): Float
     fun mean(): Float
     fun standardDeviation(): Float
 }
 
-class Linear(private val a: Float, private val b: Float): Distribution {
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+class Continuous(private val a: Float, private val b: Float): Distribution {
+    init {
+        require(a < b) {
+            "First node must be lower than second"
+        }
     }
-
-    override fun probabilityDensityFunction(x: Int) = a * x + b
-    override fun mean() = 0f
-    override fun standardDeviation() = 0f
+    override fun takeRange() = (0..100).map {
+        (a - 10) + it / ((b + 10) - (a - 10))
+    }
+    override fun probabilityDensityFunction(x: Float) =
+        if(x < a || x > b) 0f else 1 / (b - a)
+    override fun mean() = (a + b) / 2
+    override fun standardDeviation() = (b - a) / sqrt(12f)
 }
 
 class Binomial(private val p: Float, private val n: Int): Distribution {
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+    init {
+        require(p in 0.0..1.0 && n >= 0) {
+            "Probability must be in range [0,1] and amount must be positive"
+        }
     }
-
-    override fun probabilityDensityFunction(x: Int): Float {
+    override fun takeRange() = (0..100).map {
+        1f * it / n
+    }
+    override fun probabilityDensityFunction(x: Float): Float {
         val nf = (1..n).reduce { v, w -> v * w }
-        val xf = (1..x).reduce { v, w -> v * w }
-        val nkf = (1 .. (n - x)).reduce { v, w -> v * w }
+        val xf = (1..x.toInt()).reduce { v, w -> v * w }
+        val nkf = (1 .. (n - x.toInt())).reduce { v, w -> v * w }
         return nf / (xf * nkf) * p.pow(x) * (1 - p).pow(n - x)
     }
     override fun mean() = n * p
@@ -35,47 +44,61 @@ class Binomial(private val p: Float, private val n: Int): Distribution {
 }
 
 class Geometric(private val p: Float): Distribution {
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+    init {
+        require(p in 0.0..1.0) {
+            "Probability must be in range [0,1]"
+        }
     }
-
-    override fun probabilityDensityFunction(x: Int) =
+    override fun takeRange() = (1..100).map {
+        it * p
+    }
+    override fun probabilityDensityFunction(x: Float) =
         p * (1 - p).pow(x - 1)
     override fun mean() = 1 / p
     override fun standardDeviation() = sqrt(1 - p) / p
 }
 
 class Exponential(private val lambda: Float): Distribution {
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+    init {
+        require(lambda > 0) {
+            "Lambda parameter must be greater than 0"
+        }
     }
-
-    override fun probabilityDensityFunction(x: Int) = lambda * exp(-lambda * x)
+    override fun takeRange() = (1..100).map {
+        it / (lambda * 5)
+    }
+    override fun probabilityDensityFunction(x: Float) = lambda * exp(-lambda * x)
     override fun mean() = 1 / lambda
     override fun standardDeviation() = 1 / lambda
 }
 
 class Poisson(private val lambda: Float): Distribution {
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+    init {
+        require(lambda > 0) {
+            "Lambda parameter must be greater than 0"
+        }
     }
-
-    override fun probabilityDensityFunction(x: Int): Float {
-        val xf = (1..x).reduce { v, w -> v * w }
+    override fun takeRange() = (1..100).map {
+        it / (2 * lambda)
+    }
+    override fun probabilityDensityFunction(x: Float): Float {
+        val xf = (1..x.toInt()).reduce { v, w -> v * w }
         return lambda.pow(x) * exp(-lambda) / xf
     }
     override fun mean() = lambda
     override fun standardDeviation() = sqrt(lambda)
 }
 
-class Pareto(xm_i: Float, alpha_i: Float) : Distribution {
-    private var xm: Float = if (xm_i > 0) xm_i else 1f
-    private var alpha: Float = if (alpha_i > 0) alpha_i else 1f
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+class Pareto(private val xm: Float, private val alpha: Float) : Distribution {
+    init {
+        require(xm > 0 && alpha > 0) {
+            "Mode and alpha parameter must be higher than 0"
+        }
     }
-
-    override fun probabilityDensityFunction(x: Int) =
+    override fun takeRange() = (0..100).map {
+       1 + it * alpha / 10
+    }
+    override fun probabilityDensityFunction(x: Float) =
         alpha * xm.pow(alpha) / x.toFloat().pow(alpha + 1)
     override fun mean() =
         if (alpha <= 1) Float.POSITIVE_INFINITY else alpha * xm / (alpha - 1)
@@ -83,87 +106,56 @@ class Pareto(xm_i: Float, alpha_i: Float) : Distribution {
         if (alpha <= 2) Float.POSITIVE_INFINITY else xm * sqrt(alpha) / ((alpha - 1) * sqrt(alpha - 2))
 }
 
-
-class Gauss(private val u: Float, private val s: Float): Distribution {
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
-    }
-
-    override fun probabilityDensityFunction(x: Int) =
-        (1 / s * sqrt(2 * PI) * exp( -0.5 * ((x - u) / s).pow(2)))
-            .toFloat()
-    override fun mean() = u
-    override fun standardDeviation() = s
-}
-
-
-class LogNormal(private val u: Float, private val s: Float): Distribution {
+class Gauss(private val mean: Float, private val sigma: Float): Distribution {
     init {
-        require(s > 0) {
+        require(sigma > 0) {
             "Sigma must be higher than 0"
         }
     }
-
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+    override fun takeRange() = (0..100).map {
+        (mean - 4 * sigma) + it / (8 * sigma)
     }
-
-    override fun probabilityDensityFunction(x: Int) =
-        (1 / (x * s * sqrt(2 * PI.toFloat()))) *  exp(-1 * (ln(x.toFloat()) - u).pow(2) / (2 * s.pow(2)))
-    override fun mean() = exp(u + s.pow(2) / 2)
-    override fun standardDeviation() =
-        sqrt(exp(s.pow(2) - 1) * exp(2 * u + s.pow(2)))
+    override fun probabilityDensityFunction(x: Float) =
+        (1 / sigma * sqrt(2 * PI) * exp( -0.5 * ((x - mean) / sigma).pow(2)))
+            .toFloat()
+    override fun mean() = mean
+    override fun standardDeviation() = sigma
 }
 
-class Gumbel(private val u: Float, private val b: Float): Distribution {
+class LogNormal(private val mean: Float, private val sigma: Float): Distribution {
     init {
-        require(b > 0) {
+        require(sigma > 0) {
+            "Sigma must be higher than 0"
+        }
+    }
+    override fun takeRange() = (0..100).map {
+        it / (50 * sigma)
+    }
+    override fun probabilityDensityFunction(x: Float) =
+        (1 / (abs(x) * sigma * sqrt(2 * PI.toFloat()))) *
+                exp(-1 * (ln(abs(x)) - mean).pow(2) / (2 * sigma.pow(2)))
+    override fun mean() = exp(mean + sigma.pow(2) / 2)
+    override fun standardDeviation() =
+        sqrt(exp(sigma.pow(2) - 1) * exp(2 * mean + sigma.pow(2)))
+}
+
+class Gumbel(private val mean: Float, private val beta: Float): Distribution {
+    init {
+        require(beta > 0) {
             "Beta must be higher than 0"
         }
     }
-
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
+    override fun takeRange() = (0..100).map {
+        it / (beta * 10)
     }
-
-    override fun probabilityDensityFunction(x: Int): Float {
-        val z = (x - u) / b
-        return (1 / b) * exp ( -1 * (z + exp(-z)))
+    override fun probabilityDensityFunction(x: Float): Float {
+        val z = (x - mean) / beta
+        return (1 / beta) * exp ( -1 * (z + exp(-z)))
     }
     override fun mean(): Float {
         val eulerMascheroni = 0.57721f
-        return u + b * eulerMascheroni
+        return mean + beta * eulerMascheroni
     }
-    override fun standardDeviation() = PI.toFloat() * b / 6
+    override fun standardDeviation() = PI.toFloat() * beta / 6
 }
-
-class MaxwellBoltzmann(): Distribution {
-    var E = 0
-    var g = 0
-    var n = 0
-    var Z = 1
-    var T = 300
-    var u = 1
-    override fun genRange(): FloatArray {
-        TODO("Not yet implemented")
-    }
-
-    override fun probabilityDensityFunction(x: Int): Float {
-        TODO("Not yet implemented")
-    }
-
-    override fun mean(): Float {
-        TODO("Not yet implemented")
-    }
-
-    override fun standardDeviation(): Float {
-        TODO("Not yet implemented")
-    }
-}
-
-
-class BoseEinstein()
-
-class FermiDirac()
-
 
