ee9ae49d156eda6bc961dee4adea92d6b1dc1ee8
exaggeratedrumors <exaggeratedrumors@gmail.com>
2023-01-30 13:47:43 +0100

    PlotPane

--- src/jvmMain/kotlin/gui/PlotPane.kt
+++ src/jvmMain/kotlin/gui/PlotPane.kt
@@ -1,5 +1,30 @@
 package gui
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import model.Distribution
+
+//class for frame, labels and grid of parameters taken from graph
 class PlotPane {
-    //class for frame, labels and grid of parameters taken from graph
+    @Composable
+    fun createPlotPane(distribution: Distribution) {
+        val graph = Graph()
+        Column(modifier = Modifier
+            .padding(10.dp)
+            .border(width = 1.dp, color = Color.Black)
+            .padding(5.dp)
+            .width(IntrinsicSize.Min)) {
+            Row(
+                modifier = Modifier.height(IntrinsicSize.Min)
+            ) {
+                graph.printChart(distribution)
+                graph.printVerticalLabels()
+            }
+            graph.printHorizontalLabels()
+        }
+    }
 }
