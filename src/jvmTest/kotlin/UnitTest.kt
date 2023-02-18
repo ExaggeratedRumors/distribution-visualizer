da128fcef6d435a43ee35866c4d51881e4c51c91
exaggeratedrumors <exaggeratedrumors@gmail.com>
2023-02-18 16:36:47 +0100

    Tests

--- src/jvmTest/kotlin/UnitTest.kt
+++ src/jvmTest/kotlin/UnitTest.kt
@@ -5,14 +5,14 @@ import gui.*
 class UnitTest {
     @Test
     fun `grid data create`() {
-        val pp = Graph()
-        val o1 = pp.discretization(0.5f,0.75f)
+        val graph = Graph()
+        val o1 = graph.discretization(0.5f,0.75f)
         val e1 = listOf(0.5f, 0.6f, 0.7f)
         assertEquals(e1, o1)
-        val o2 = pp.discretization(10.5f,35.5f)
+        val o2 = graph.discretization(10.5f,35.5f)
         val e2 = listOf(20f, 30f)
         assertEquals(e2, o2)
-        val o3 = pp.discretization(0.004f,1f)
+        val o3 = graph.discretization(0.004f,1f)
         val e3 = listOf(0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f)
         assertEquals(e3, o3)
     }
